782573736cadf0d1053c1153aa1b36266577aa2e
Sujay <sujaydamani98@gmail.com>
2019-11-07 20:59:15 -0800

    portfolio

---
+++ build/precache-manifest.da2c6f4638888dcdf4c0c7625bb6808b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f05fb195cf70ea4598726794ae0d2f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22c3aaee3c6f79949f75",
+    "url": "./static/css/2.f8801741.chunk.css"
+  },
+  {
+    "revision": "6a2711ee091eac618f0b",
+    "url": "./static/css/main.b7c5f97a.chunk.css"
+  },
+  {
+    "revision": "22c3aaee3c6f79949f75",
+    "url": "./static/js/2.556bf47c.chunk.js"
+  },
+  {
+    "revision": "6a2711ee091eac618f0b",
+    "url": "./static/js/main.f6c92d67.chunk.js"
+  },
+  {
+    "revision": "c6b730ee3081cb70f23b",
+    "url": "./static/js/runtime-main.e542653f.js"
+  },
+  {
+    "revision": "5893b23dc9e07441811584616d487e94",
+    "url": "./static/media/Sujay-Resume.5893b23d.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "69a339d4eaaa2c7617d3be5d3ab17ac5",
+    "url": "./static/media/myAvatar.69a339d4.png"
+  }
+]);
